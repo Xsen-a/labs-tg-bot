b3fee2e8fdd551ae0d62bbea11dbbf8c4a710017
Xsen-a <malikina2003@mail.ru>
2025-03-12 01:41:43 +0300

    Создана база данных

--- api/alembic/script.py.mako
+++ api/alembic/script.py.mako
@@ -9,7 +9,6 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-import sqlmodel # added
 ${imports if imports else ""}
 
 # revision identifiers, used by Alembic.
@@ -20,8 +19,10 @@ depends_on: Union[str, Sequence[str], None] = ${repr(depends_on)}
 
 
 def upgrade() -> None:
+    """Upgrade schema."""
     ${upgrades if upgrades else "pass"}
 
 
 def downgrade() -> None:
+    """Downgrade schema."""
     ${downgrades if downgrades else "pass"}
--- api/src/models.py
+++ api/src/models.py
@@ -5,7 +5,7 @@ import enum
 from typing import Optional
 
 from sqlalchemy import MetaData
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from datetime import time, date
 
 metadata = MetaData()
@@ -25,6 +25,12 @@ class User(SQLModel, table=True):
     is_petrsu_student: bool = Field(default=False, description="Флаг, является ли пользователь студентом ПетрГУ")
     group: Optional[str] = Field(default=None, max_length=100, description="Группа студента ПетрГУ")
 
+    disciplines: list["Discipline"] = Relationship(back_populates="user")
+    teachers: list["Teacher"] = Relationship(back_populates="user")
+    tasks: list["Task"] = Relationship(back_populates="user")
+    files: list["File"] = Relationship(back_populates="user")
+    lessons: list["Lesson"] = Relationship(back_populates="user")
+
 
 class Teacher(SQLModel, table=True):
     __tablename__ = "tblTeacher"
@@ -37,41 +43,56 @@ class Teacher(SQLModel, table=True):
     classroom: Optional[str] = Field(default=None, max_length=100, description="Аудитория, в которой можно найти преподавателя")
     is_from_API: bool = Field(default=False, description="Флаг, получен ли преподаватель из API ПетрГУ")
 
+    user: User = Relationship(back_populates="teacher")
+    disciplines: list["Discipline"] = Relationship(back_populates="teacher")
 
-class Dicipline(SQLModel, table=True):
-    __tablename__ = "tblDicipline"
-    dicipline_id: int = Field(default=None, primary_key=True, description="Уникальный идентификатор записи о дисциплине")
+
+class Discipline (SQLModel, table=True):
+    __tablename__ = "tblDiscipline"
+    discipline_id: int = Field(default=None, primary_key=True, description="Уникальный идентификатор записи о дисциплине")
     user_id: int = Field(foreign_key="tblUser.user_id", description="Ссылка на идентификатор записи о пользователе")
     teacher_id: Optional[int] = Field(foreign_key="tblTeacher.teacher_id", description="Ссылка на идентификатор записи о преподавателе")
     name: str = Field(max_length=100, description="Название дисциплины")
     is_from_API: bool = Field(default=False, description="Флаг, получена ли дисциплина из API ПетрГУ")
+
+    user: User = Relationship(back_populates="disciplines")
+    teacher: Optional["Teacher"] = Relationship(back_populates="disciplines")
+    lessons: list["Lesson"] = Relationship(back_populates="discipline")
+    tasks: list["Task"] = Relationship(back_populates="discipline")
 
 
 class Lesson(SQLModel, table=True):
     __tablename__ = "tblLesson"
     lesson_id: int = Field(default=None, primary_key=True, description="Уникальный идентификатор записи о паре")
     user_id: int = Field(foreign_key="tblUser.user_id", description="Ссылка на идентификатор записи о пользователе")
-    dicipline_id: int = Field(foreign_key="tblDicipline.dicipline_id", description="Ссылка на идентификатор записи о дисциплине")
+    discipline_id: int = Field(foreign_key="tblDiscipline.discipline_id", description="Ссылка на идентификатор записи о дисциплине")
     classroom: str = Field(max_length=100, description="Аудитория, в которой проходит пара")
     start_time: time = Field(description="Время начала пары")
     end_time: time = Field(description="Время окончания пары")
-    date: date = Field(description="Дата проведения первой периодичной или единичной пары")
+    start_date: date = Field(description="Дата проведения первой периодичной или единичной пары")
     periodicity_days: int = Field(default=0, description="Периодичность в днях, с которой повторяются пары")
     is_from_API: bool = Field(default=False, description="Флаг, получена ли пара из API")
+
+    user: User = Relationship(back_populates="lessons")
+    discipline: Discipline = Relationship(back_populates="lessons")
 
 
 class Task(SQLModel, table=True):
     __tablename__ = "tblTask"
     task_id: int = Field(default=None, primary_key=True, description="Уникальный идентификатор записи о лабораторной работе")
     user_id: int = Field(foreign_key="tblUser.user_id", description="Ссылка на идентификатор записи о пользователе")
-    dicipline_id: int = Field(foreign_key="tblDicipline.dicipline_id", description="Ссылка на идентификатор записи о дисциплине")
+    discipline_id: int = Field(foreign_key="tblDiscipline.discipline_id", description="Ссылка на идентификатор записи о дисциплине")
     name: str = Field(max_length=100, description="Название лабораторной работы")
     task_text: Optional[str] = Field(default=None, description="Текст задания лабораторной работы")
     task_link: Optional[str] = Field(default=None, max_length=255, description="Ссылка на задание лабораторной работы")
     start_date: date = Field(description="Дата начала выполнения лабораторной работы")
     end_date: date = Field(description="Дата сдачи лабораторной работы")
     extra_info: Optional[str] = Field(default=None, description="Дополнительная информация о лабораторной работе")
-    status: Status = Field(description="Статус выполнения лабораторной работы")
+    status: Status = Field(default=Status.not_started, description="Статус выполнения лабораторной работы")
+
+    user: User = Relationship(back_populates="tasks")
+    discipline: Discipline = Relationship(back_populates="tasks")
+    files: list["File"] = Relationship(back_populates="task")
 
 
 class File(SQLModel, table=True):
@@ -80,4 +101,7 @@ class File(SQLModel, table=True):
     user_id: int = Field(foreign_key="tblUser.user_id", description="Ссылка на идентификатор записи о пользователе")
     task_id: int = Field(foreign_key="tblTask.task_id", description="Ссылка на идентификатор записи о лабораторной работе")
     file_name: str = Field(max_length=255, description="Имя файла")
-    file_data: bytearray = Field(description="Содержимое файла любого формата в байтах")
+    file_data: bytes = Field(description="Содержимое файла любого формата в байтах")
+
+    user: User = Relationship(back_populates="files")
+    task: Task = Relationship(back_populates="files")
